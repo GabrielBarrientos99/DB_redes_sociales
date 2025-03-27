d6ac272b5bfa977baf1d1378522d30aef69cb081
GabrielBarrientos99 <gabriel.barrientos.c@uni.pe>
2025-03-27 18:41:39 -0500

    a

--- TRABAJO04/Modelo Dimensional.sql
+++ TRABAJO04/Modelo Dimensional.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE BI_red_social;
 USE BI_red_social;
 GO
+DROP DATABASE BI_red_social;
 
 CREATE TABLE dim_usuarios(
     id INT IDENTITY(1,1) PRIMARY KEY,
@@ -14,6 +15,8 @@ CREATE TABLE dim_publicaciones(
 	id INT IDENTITY(1,1) PRIMARY KEY,
 	id_publicacion INT NOT NULL,
 	id_usuario INT NOT NULL,
+	id_likes INT NOT NULL,
+	id_comentario INT NOT NULL,
 	contenido TEXT,
 );
 
@@ -25,36 +28,19 @@ CREATE TABLE dim_fecha (
     dia INT NOT NULL
 );
 
--- Crear dimensión para tipos de interacción
-CREATE TABLE dim_tipo_interaccion (
-    id_tipo INT PRIMARY KEY,
-    nombre_tipo VARCHAR(20) NOT NULL
-);
-
--- Insertar tipos de interacción básicos
-INSERT INTO dim_tipo_interaccion (id_tipo, nombre_tipo)
-VALUES
-    (1, 'LIKE'),
-    (2, 'COMMENT'),
-    (3, 'REPLY'),
-    (4, 'FOLLOW');
 
 CREATE TABLE fact_interaccion (
-    interaccion_id INT IDENTITY(1,1) PRIMARY KEY,
+    id INT IDENTITY(1,1) PRIMARY KEY,
     usuario_id INT NOT NULL,
     publicacion_id INT NULL,
     usuario_followed_id INT NULL,
     fecha_id INT NOT NULL,
-    tipo_interaccion_id INT NOT NULL,
     cantidad INT DEFAULT 1,
-    -- Degenerate dimensions para referencias opcionales
-    id_like INT NULL,
-    id_comentario INT NULL,
-    id_seguimiento INT NULL,
+  
 
     FOREIGN KEY (usuario_id) REFERENCES dim_usuarios(id),
     FOREIGN KEY (publicacion_id) REFERENCES dim_publicaciones(id),
     FOREIGN KEY (usuario_followed_id) REFERENCES dim_usuarios(id),
     FOREIGN KEY (fecha_id) REFERENCES dim_fecha(id),
-    FOREIGN KEY (tipo_interaccion_id) REFERENCES dim_tipo_interaccion(id_tipo)
 );
+
